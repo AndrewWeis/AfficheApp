e28c8a945368c3b5cd99eb39b58d330e58ccbc71
Ilya Volf <volfilya@gmail.com>
2023-08-21 15:44:58 +0700

    Location service has been removed, Android Gradle Plugin version has been changed

--- app/src/main/java/scientists/house/affiche/app/screens/main/tabs/news/NewsPostDetailsFragment.kt
+++ app/src/main/java/scientists/house/affiche/app/screens/main/tabs/news/NewsPostDetailsFragment.kt
@@ -1,15 +1,14 @@
 package scientists.house.affiche.app.screens.main.tabs.news
 
-import android.os.Bundle
-import android.view.View
 import androidx.navigation.fragment.navArgs
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 import scientists.house.affiche.app.R
 import scientists.house.affiche.app.databinding.FragmentNewsPostDetailsBinding
-import scientists.house.affiche.app.model.DataHolder
+import scientists.house.affiche.app.utils.DataHolder
 import scientists.house.affiche.app.model.news.entities.NewsDetailedPost
 import scientists.house.affiche.app.screens.base.BaseFragment
+import scientists.house.affiche.app.utils.viewBinding
 import scientists.house.affiche.app.utils.viewModelCreator
 import scientists.house.affiche.app.utils.visible
 
@@ -22,15 +21,9 @@ class NewsPostDetailsFragment : BaseFragment(R.layout.fragment_news_post_details
         factory.create(args.postLink)
     }
 
-    private lateinit var binding: FragmentNewsPostDetailsBinding
+    private val binding by viewBinding<FragmentNewsPostDetailsBinding>()
 
     private val args by navArgs<NewsPostDetailsFragmentArgs>()
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding = FragmentNewsPostDetailsBinding.bind(view)
-        super.onViewCreated(view, savedInstanceState)
-    }
-
     override fun setupViews() {
         super.setupViews()
         binding.apply {
